95870b02a3a2e6b8e156947f424ea8e27b4f52ea
Augusto L <juanaugustolombino@protonmail.ch>
2024-06-14 11:48:46 -0300

    add winston-daily-rotate-file and create a logger utils file

---
+++ src/utils/logger.js
@@ -0,0 +1,29 @@
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf, errors } = format;
+const DailyRotateFile = require('winston-daily-rotate-file');
+
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+  return `${timestamp} ${level}: ${stack || message}`;
+});
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(
+    format.colorize(),
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    errors({ stack: true }),
+    logFormat
+  ),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: 'logs/qr-generator-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+});
+
+module.exports = logger;
